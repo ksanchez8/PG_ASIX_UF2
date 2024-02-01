74397886a7b0fa27137bd949e147c74805283101
ksanchez8 <ksanchez@institutmvm.cat>
2024-02-01 09:50:39 +0100

    Introducción de enunciado comentado

---
+++ pizza_montalban.py
@@ -0,0 +1,5 @@
+"""
+Imagineu-vos que heu estat contractats per dissenyar el sistema de gestió d'una pizzeria. 
+L'objectiu és desenvolupar un conjunt de funcions que permetin gestionar diferents tasques relacionades amb el funcionament de la pizzeria. Això inclou la presa de comandes, la preparació de pizzes, la gestió de l'estoc d'ingredients i el processament dels pagaments.
+Recordeu fer servir disseny top down, i mantenir un baix acoblament i una alta cohesió.
+"""
