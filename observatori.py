6f492bb9be0d86af4c629559a62063eccc934008
ksanchez8 <ksanchez@institutmvm.cat>
2024-02-05 13:28:34 +0100

    Creació del def mostrar_menu

--- observatori.py
+++ observatori.py
@@ -1,1 +1,23 @@
-#
+# Programa principal / Mostrar menú
+def mostrar_menu(temperatures):
+    for setmana in range(1, 53):
+        print(f"\n----- Setmana {setmana} de l'any -----")
+        print("\nBenvingut al registre de temperatures")
+        print("-------------------------------------")
+        print("[RT] Registrar temperatures setmanals.")
+        print("[MJ] Consultar temperatura mitjana.")
+        print("[DF] Consultar diferència màxima.")
+        print("[FI] Sortir.")
+
+        opcio = input("Opció: ")
+
+        if opcio == 'RT':
+            registrar_temperatures(temperatures)
+        elif opcio == 'MJ':
+            consultar_temperatura_mitjana(temperatures)
+        elif opcio == 'DF':
+            consultar_diferencia_maxima(temperatures)
+        elif opcio == 'FI':
+            break
+        else:
+            print("Opció no vàlida. Torna a intentar.")
