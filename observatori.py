949e544d807de38e3a870f489d5d049d675e4834
ksanchez8 <ksanchez@institutmvm.cat>
2024-02-06 10:51:42 +0100

    Intoducció del codi per poder introduir les dades a temperatura tipo flotants

--- observatori.py
+++ observatori.py
@@ -2,6 +2,7 @@
 temperatures = []
 def registrar_temperatures(temperatures):
     temperatures_input = input("Introdueix les temperatures de la setmana anterior: ")
+    temperatura_setmanal = [float(temp.replace(',', '.')) for temp in temperatures_input.split()]
 
 def mostrar_menu(temperatures):
     
