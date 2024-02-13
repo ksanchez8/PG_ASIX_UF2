061e3398aff5078926e0c9484bfb000cfc7dcb12
ksanchez8 <ksanchez@institutmvm.cat>
2024-02-13 10:17:49 +0100

    Datos

---
+++ observatori_albert.py
@@ -0,0 +1,26 @@
+#Registre de temperatures
+
+def mostrarMenu():
+
+def tractarOpcio():
+
+def mostrarTemperaturesSetmanas():
+
+def mostrarMitjana():
+
+def mostrarDiferencia():
+
+def finalitzarExecucio():
+
+def llegir_temperatures_teclat():
+    lector = input("Escriu les temperatures d'aquesta setamana: ")
+    for temperatura in lector.split():
+        temperatures.append(float(temperatura.replace(',','.')))
+
+def calcula_diferencia():
+
+def calculaMitjana():
+
+def mostrar_data():
+
+def incrementar_data():
