9fed245f0898cae9e2156e8aa2f5f723aaf9599b
ksanchez8 <ksanchez@institutmvm.cat>
2024-02-16 09:28:40 +0100

    Se introduce return

--- FraseAmbMesAs.py
+++ FraseAmbMesAs.py
@@ -1,6 +1,6 @@
 def frase():
     introducir_frase = input("Escriu la frase: ")
-    print(introducir_frase)
+    return introducir_frase
 
 def contar_as():
     frase_minusculas = introducir_frase.casefold().count('a')
