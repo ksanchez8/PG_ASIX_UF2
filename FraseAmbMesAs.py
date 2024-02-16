077676e1bb12cd786ae6e59b3f330adb367becd6
ksanchez8 <ksanchez@institutmvm.cat>
2024-02-16 09:42:32 +0100

    Se ha creado dos def, uno de salida y otro de contar

--- FraseAmbMesAs.py
+++ FraseAmbMesAs.py
@@ -1,7 +1,21 @@
 def contar_as():
-    introducir_frase = input("Escriu la frase: ")
-    frase_minusculas = introducir_frase.casefold().count('a')
+    global fi
+    fi = False
+
+    while not fi:
+        introducir_frase = input("Escriu la frase: ")
+        
     
+        if introducir_frase == "fi":
+            salida()
+
+        else:
+            frase_minusculas = introducir_frase.casefold().count('a')
+            print(frase_minusculas)
+
+def salida():
+    global fi
+    fi = True
 
 
 contar_as()
