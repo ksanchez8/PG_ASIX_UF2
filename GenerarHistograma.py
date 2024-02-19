da0dc840b9644f3a9cbe6d1217ff1c9ecaae7b33
ksanchez8 <ksanchez@institutmvm.cat>
2024-02-19 13:01:37 +0100

    Camvi de variable de maxim tirades

--- GenerarHistograma.py
+++ GenerarHistograma.py
@@ -1,8 +1,8 @@
-def contar_repeticiones(num_lanzamientos):
+def contar_repeticiones(maxim_tirades):
     for dado1 in range(1, 7):
         for dado2 in range(1, 7):
             suma = dado1 + dado2
             print(f"{dado1} + {dado2} ES {suma}")
 
-num_lanzamientos = 36
+maxim_tirades = 36
 contar_repeticiones(num_lanzamientos)
