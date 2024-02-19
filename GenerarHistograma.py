cb336397496d3ace38a114cefe193403c5fd1926
ksanchez8 <ksanchez@institutmvm.cat>
2024-02-19 13:08:52 +0100

    Correció

--- GenerarHistograma.py
+++ GenerarHistograma.py
@@ -1,12 +1,15 @@
 resultados = []
+maxim_tirades = 36
 
 def contar_repeticiones(maxim_tirades):
     for dado1 in range(1, 7):
         for dado2 in range(1, 7):
             suma = dado1 + dado2
             print(f"{dado1} + {dado2} ES {suma}")
-            resultados = suma
-            return resultados
+            resultados.append(suma)
 
-maxim_tirades = 36
+    return resultados
+            
+
+
 contar_repeticiones(maxim_tirades)
