bda28172da302d52f625cd71a0fd82654564c5ee
ksanchez8 <ksanchez@institutmvm.cat>
2024-02-19 13:19:50 +0100

    Es modifica el nom de la variable

--- GenerarHistograma.py
+++ GenerarHistograma.py
@@ -8,9 +8,9 @@ def contar_repeticiones(maxim_tirades):
             print(f"{dado1} + {dado2} ES {suma}")
             resultados.append(suma)
 
-    for i in range(2, 13):
-        repeticiones = resultados.count(i)
-        print(f"La suma {i} se repite {repeticiones} veces.")
+    for quantitat in range(2, 13):
+        repeticiones = resultados.count(quantitat)
+        print(f"La suma {quantitat} se repite {repeticiones} veces.")
 
     return resultados
             
