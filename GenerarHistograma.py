b894d12c0547fd6d3a529bb19b480d924b71ec42
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-02-19 23:21:40 +0100

    Se añaden pre y post

--- GenerarHistograma.py
+++ GenerarHistograma.py
@@ -1,3 +1,7 @@
+#/PRE: Feu un programa anomenat GenerarHistograma que mostri l’histograma de totes les tirades possibles amb dos daus de sis cares. 
+#Per fer això, l’estratègia que s’ha considerat és desar dins d’un  array el nombre de repeticions de cada tirada possible (sempre entre 2 i 12) 
+#i després treballar a partir d’aquests valors. 
+
 resultados = [] # Lista para almacenar los resultados de las tiradas
 maxim_tirades = 36 # Número máximo de tiradas
 
@@ -20,10 +24,12 @@ def contar_repeticiones(maxim_tirades):
         if repeticiones > max_repeticiones:
             max_repeticiones = repeticiones
             num_max_repeticiones = quantitat
-            
+
     # Imprimir el resultado con el número que más se repite y cuántas veces
     print(f"El numero que mas se repite es: {num_max_repeticiones}, que se repite {max_repeticiones} veces.")
 
     return resultados
 
 contar_repeticiones(maxim_tirades)
+
+#POST:A continuació, ha de dir quin valor de tirada és el que té més repeticions. 
