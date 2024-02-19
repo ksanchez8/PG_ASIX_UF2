a148efb3654a75fa67bd330ce2d7488677f866ed
ksanchez8 <ksanchez@institutmvm.cat>
2024-02-19 12:58:16 +0100

    Prova de bucle

--- GenerarHistograma.py
+++ GenerarHistograma.py
@@ -1,7 +1,8 @@
 def contar_repeticiones(num_lanzamientos):
-    
     for dado1 in range(1, 7):
         for dado2 in range(1, 7):
             suma = dado1 + dado2
-            print(suma)
-    
+            print(f"{dado1} + {dado2} ES {suma}")
+
+num_lanzamientos = 100
+contar_repeticiones(num_lanzamientos)
