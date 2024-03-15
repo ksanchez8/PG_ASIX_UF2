642762a6c1840ebe6fae5cb7f6f717d7674208d3
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-03-15 08:27:58 +0100

    Classe per representar un soci

--- biblioteca.kt
+++ biblioteca.kt
@@ -4,3 +4,10 @@ class Llibre(val titol: String, val autor: String, var exemplarsDisponibles: Int
         return "Títol: $titol, Autor: $autor, Exemplars disponibles: $exemplarsDisponibles"
     }
 }
+
+// Classe per representar un soci
+class Soci(val nom: String, val cognom: String, val numSoci: Int) {
+    fun obtenirInformacio(): String {
+        return "Nom: $nom, Cognom: $cognom, Número de soci: $numSoci"
+    }
+}
