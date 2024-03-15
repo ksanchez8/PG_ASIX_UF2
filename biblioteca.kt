1b0761ff6d63284fdf50c2dd61430b1fe100cc98
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-03-15 09:07:45 +0100

    Creació de funcio principal

--- biblioteca.kt
+++ biblioteca.kt
@@ -11,3 +11,16 @@ class Soci(val nom: String, val cognom: String, val numSoci: Int) {
         return "Nom: $nom, Cognom: $cognom, Número de soci: $numSoci"
     }
 }
+
+// Classe per representar un préstec
+class Prestec(val llibre: Llibre, val soci: Soci, val dataPrestec: String) {
+    fun obtenirInformacio(): String {
+        return "Llibre: ${llibre.titol}, Soci: ${soci.nom} ${soci.cognom}, Data de préstec: $dataPrestec"
+    }
+}
+
+// Función principal
+fun main() {
+    // Creación de libros
+    val libro1 = Libro("El señor de los anillos", "J.R.R. Tolkien", 3)
+    val libro2 = Libro("Crimen y castigo", "Fyodor Dostoevsky", 5)
