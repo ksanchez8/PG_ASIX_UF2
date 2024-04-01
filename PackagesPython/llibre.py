81604913a689eb2d440adda40218ddce7a6ab73c
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-04-01 19:55:26 +0200

    Se crea un def para hacer un return de la información introducida por teclado

--- PackagesPython/llibre.py
+++ PackagesPython/llibre.py
@@ -3,3 +3,6 @@ class Llibre:
         self.titol = input("Introduce el título del libro: ")
         self.autor = input("Introduce el autor del libro: ")
         self.exemplars = int(input("Introduce el número de ejemplares disponibles: "))
+
+     def informacio(self):
+        return f"Título: {self.titol}, Autor: {self.autor}, Ejemplares disponibles: {self.exemplars}"
