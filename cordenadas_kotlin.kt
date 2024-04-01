bf90608311d3f14f6e33b354332b15dd94fb9620
Kilian <ksanchez@institutmvm.cat>
2024-04-01 18:47:20 +0200

    Creación de comentario con el codigo en python

---
+++ cordenadas_kotlin.kt
@@ -0,0 +1,27 @@
+/*
+Hay que realizar el siguiente codigo en kotlin:
+class Coordenada:
+    def __init__(self, x=0, y=0) -> None:
+        self.x: int = x
+        self.y: int = y
+def moure_dreta(coordenada: Coordenada) -> Coordenada:
+    return Coordenada(coordenada.x + 1, coordenada.y)
+def moure_esquerra(coordenada: Coordenada) -> Coordenada:
+    return Coordenada(coordenada.x - 1, coordenada.y)
+def moure_amunt(coordenada: Coordenada) -> Coordenada:
+    return Coordenada(coordenada.x, coordenada.y + 1)
+def moure_avall(coordenada: Coordenada) -> Coordenada:
+    return Coordenada(coordenada.x, coordenada.y - 1)
+class Main:
+    def __init__(self) -> None:
+        coordenada: Coordenada = Coordenada()
+        nova_coordenada = moure_dreta(coordenada)
+        print(f"Nova coordenada despres de moure a la dreta: ({nova_coordenada.x}, {nova_coordenada.y})")
+
+        nova_coordenada = moure_amunt(nova_coordenada)
+        print(f"Nova coordenada despres de moure amunt: ({nova_coordenada.x}, {nova_coordenada.y})") 
+        print(f"Nova coordenada despres de moure amunt: ({nova_coordenada.x}, {nova_coordenada.y})")
+
+
+Main()
+ */
