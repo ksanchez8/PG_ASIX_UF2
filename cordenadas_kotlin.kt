30469018353ea49a121ffcb275a9d82b82874566
Kilian <ksanchez@institutmvm.cat>
2024-04-01 18:58:22 +0200

    Creació de fun moureAmunt

--- cordenadas_kotlin.kt
+++ cordenadas_kotlin.kt
@@ -35,3 +35,7 @@ fun moureDreta(coordenada: Coordenada): Coordenada {
 fun moureEsquerra(coordenada: Coordenada): Coordenada {
     return Coordenada(coordenada.x - 1, coordenada.y)
 }
+
+fun moureAmunt(coordenada: Coordenada): Coordenada {
+    return Coordenada(coordenada.x, coordenada.y + 1)
+}
