7d77ba25d0cfb17da2c19f5ebf9e96b57a93c869
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-04-01 20:15:54 +0200

    Solicita al usuario que introduzca el número de socio y lo asigna a la variable 'num_soci'

--- PackagesPython/soci.py
+++ PackagesPython/soci.py
@@ -2,3 +2,4 @@ class Soci:
     def __init__(self):
         self.nom = input("Introduce el nombre del socio: ")
         self.cognom = input("Introduce el apellido del socio: ")
+        self.num_soci = input("Introduce el número de socio: ")
