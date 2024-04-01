e7491bc9dd4b8f4893ea77ac52fb9c405ef9d34d
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-04-01 20:18:55 +0200

    Se almacena la informacion con el return

--- PackagesPython/prestec.py
+++ PackagesPython/prestec.py
@@ -3,3 +3,6 @@ class Prestec:
         self.llibre = llibre
         self.soci = soci
         self.data_prestec = data_prestec
+
+    def informacio(self):
+        return f"Llibre: {self.llibre.informacio()}, Socio: {self.soci.informacio()}, Fecha de préstamo: {self.data_prestec}"
