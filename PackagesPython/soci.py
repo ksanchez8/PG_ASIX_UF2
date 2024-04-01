528c4cf06033e21442f863a9733357d887f6f881
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-04-01 20:16:41 +0200

    Se almacena la información del socio con el reurn

--- PackagesPython/soci.py
+++ PackagesPython/soci.py
@@ -3,3 +3,6 @@ class Soci:
         self.nom = input("Introduce el nombre del socio: ")
         self.cognom = input("Introduce el apellido del socio: ")
         self.num_soci = input("Introduce el número de socio: ")
+
+    def informacio(self):
+        return f"Nombre: {self.nom}, Apellido: {self.cognom}, Número de socio: {self.num_soci}"
