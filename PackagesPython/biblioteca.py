06c747a1f4f773ae4328cc5030068bc97bb10246
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-04-01 20:33:13 +0200

    Creación de comentario introduciendo el enunciado

--- PackagesPython/biblioteca.py
+++ PackagesPython/biblioteca.py
@@ -1,3 +1,28 @@
+"""
+Enunciado del ejercicio:
+Gestió de Biblioteca
+
+Imagina que estàs desenvolupant un sistema de gestió per una biblioteca. Necessites crear un conjunt de classes que permetin gestionar llibres, socis i préstecs.
+
+Requisits:
+
+1. Llibre
+   - Té títol, autor i número d'exemplars disponibles.
+   - Un exemplar es pot tenir en préstec, retornar i s'ha de poder obtenir informació del llibre.
+
+2. Soci:
+   - Té nom, cognom i número de soci.
+   - Ha de poder sol·licitar un préstec, retornar un préstec i mostrar la informació del soci.
+
+3. Préstec:
+   - Té quin llibre està en préstec, el soci que el té i la data de préstec.
+   - S'ha de poder registrar el préstec, retornar el préstec i mostrar la informació del préstec.
+
+Que has de fer?
+
+Fent servir python, crea un package per fer aquest exercici. Cada Classe ha d'estar en el seu propi mòdul. Els imports han d'estar en el seu lloc.
+"""
+
 from llibre import Llibre
 from soci import Soci
 from prestec import Prestec
