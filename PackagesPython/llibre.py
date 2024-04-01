0e69dcbf9165c22f5ec5eae9d0e692b3502b866d
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-04-01 19:54:13 +0200

    Se crea la class llibre

---
+++ PackagesPython/llibre.py
@@ -0,0 +1,5 @@
+class Llibre:
+    def __init__(self):
+        self.titol = input("Introduce el título del libro: ")
+        self.autor = input("Introduce el autor del libro: ")
+        self.exemplars = int(input("Introduce el número de ejemplares disponibles: "))
